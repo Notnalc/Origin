636c8cf45dcd518265fe5b0110d4ffba490eb6f3
Healing Bussolino <hbussolino2021@elam.co.uk>
2021-11-16 15:49:54 +0000

    i did some scripts

--- New Unity Project (1)/Assets/CharacterController.cs
+++ New Unity Project (1)/Assets/CharacterController.cs
@@ -7,14 +7,24 @@ public class CharacterController : MonoBehaviour
     public float maxSpeed = 1.0f;
     float rotation = 0.0f;
     GameObject cam;
+    Rigidbody myRigidbody;
     
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
     float rotationSpeed = 2.0f;
     float camRotationSpeed = 1.5f;
     float camRotation;
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+
+
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
 
@@ -23,12 +33,25 @@ public class CharacterController : MonoBehaviour
 
     void Update()
     {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+    
+
+      
+
         transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime);
         
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
-        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));    
+        
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
+       
+       cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));    
     }
 }
